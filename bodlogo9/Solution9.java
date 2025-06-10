03466bd477d738aab256ab47480d2d4fc9478641
bazarsad <bazarsadadiya8@gmail.com>
2025-06-10 12:40:26 +0800

    Create Solution9.java

---
+++ bodlogo9/Solution9.java
@@ -0,0 +1,17 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Solution9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+
+        BigInteger factorial = BigInteger.ONE;
+        for (int i = 2; i <= n; i++) {
+            factorial = factorial.multiply(BigInteger.valueOf(i));
+        }
+
+        System.out.println(factorial);
+    }
+}
